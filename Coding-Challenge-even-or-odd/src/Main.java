61027aca456b8d4969d8e65c0c202c3a2f23ac8f
Opale <opale.m.schappert@gmail.com>
2022-09-20 16:22:49 -0600

    First challenge

---
+++ Coding-Challenge-even-or-odd/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int Testint = 98;
+
+        if ((Testint % 2) == 0){
+            System.out.println("This number is even");
+        }
+        else{
+            System.out.println("This number is odd");
+        }
+    }
+}
